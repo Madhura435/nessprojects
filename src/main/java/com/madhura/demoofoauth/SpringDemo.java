c5e01edfbf57a3fa307112f54fa8a96d98994979
Madhura453 <madhubhavi453@gmail.com>
2021-07-12 17:13:02 +0530

    OAuth2

--- src/main/java/com/madhura/demoofoauth/SpringDemo.java
+++ src/main/java/com/madhura/demoofoauth/SpringDemo.java
@@ -1,13 +1,33 @@
 package com.madhura.demoofoauth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SpringDemo extends WebSecurityConfigurerAdapter {
 @Override
 protected void configure(HttpSecurity http) throws Exception {
-	http.authorizeRequests().anyRequest().authenticated().and().oauth2Login();
+	
+	http
+	.authorizeRequests(au -> au
+		.antMatchers("/", "/error", "/webjars/**").permitAll()
+		.anyRequest().authenticated()
+	)
+	.exceptionHandling(er -> er
+		.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+	)
+	.csrf(cs -> cs
+		.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+	)
+	.logout(lo -> lo
+		.logoutSuccessUrl("/").permitAll()
+	)
+	.oauth2Login();
+	
+
 }
 }
